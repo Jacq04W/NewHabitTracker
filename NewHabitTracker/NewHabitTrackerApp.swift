af4ac122b9ef702e72d617273a71ddc4b633bc57
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2023-09-03 07:21:58 -0400

    Initial Commit

---
+++ NewHabitTracker/NewHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewHabitTrackerApp.swift
+//  NewHabitTracker
+//
+//  Created by Jacquese Whitson on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NewHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
